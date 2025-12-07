b6217789a08fed612930689032ce8ae05b74190a
Your Name <you@example.com>
2025-12-07 15:41:43 +0200

    feat: implement deck.js functions

---
+++ utils/deck.js
@@ -0,0 +1,71 @@
+function createCard(rank, suite) {
+    let value = 0;
+
+    if (isNaN(rank)) {
+
+        switch (rank) {
+            case "J":
+            value = 11;
+            break;
+
+        case "Q":
+            value = 12;
+            break;
+
+        case "K":
+            value = 13;
+            break;
+
+        case "A":
+            value = 14;
+            break;
+
+        default:
+            break;
+        }
+    } else {
+        value = Number(rank);
+    }
+
+    return {
+        rank: rank,
+        suite: suite,
+        value: value
+    }
+}
+
+
+function compareCard(p1Card, p2Card) {
+    const p1Value = p1Card.value;
+    const p2Value = p2Card.value;
+
+    let winner = '';
+
+    if (p1Value > p2Value) {
+        winner = 'p1';
+    } else if (p2Value > p1Value) {
+        winner = 'p2';
+    } else {
+        winner = 'WAR';
+    }
+
+    return winner;
+}
+
+
+function createDeck() {
+    const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const suits = ['H', 'C', 'D', 'S'];
+    const cards = []
+    
+    ranks.forEach((rank) => {
+        suits.forEach((suite) => {
+            const card = createCard(rank, suite);
+            cards.push(card);
+        })
+    })
+
+    return cards
+}
+
+console.log(createDeck().slice(0, 4));
